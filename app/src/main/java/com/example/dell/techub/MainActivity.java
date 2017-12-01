c72014b7ba15baa04412707dd09a3e3f74e3a7ac
Haitham Salama <haithamsalama92@gmail.com>
2017-12-01 14:23:42 +0200

    Initial commit

---
+++ app/src/main/java/com/example/dell/techub/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.dell.techub;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void facepage(View view){
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/Techubeg"));
+        startActivity(browserIntent);
+
+    }
+}
